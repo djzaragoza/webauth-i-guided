f94494e2fc36301e3883a99e96970ff78aca149a
unknown <dj.zaragoza@icloud.com>
2019-08-21 16:47:51 -0700

    finished dbconfig file

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,9 +2,4 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 
-const bcrypt = require('bcryptjs');
-
-const credentials = req.body;
-const hash = bcrypt.hashSync(credentials.password, 14);
-
-module.exports = knex(knexConfig.development);
+module.exports - knex(knexConfig.development);
--- index.js
+++ index.js
@@ -35,12 +35,12 @@ server.post('/api/register', (req, res) => {
 });
 
 server.post('/api/login', (req, res) => {
-  let { username, passwordGuests } = req.body;
+  let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user && bcrypt.compareSync(passwordGuests, user.password)) {
+      if (user || !bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
