57720f2ed42d3e65752da78b862d8d8dc38d987e
unknown <dj.zaragoza@icloud.com>
2019-08-20 19:33:03 -0700

    validating user password

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ server.post('/api/register', (req, res) => {
 });
 
 server.post('/api/login', (req, res) => {
-  let { username, password } = req.body;
+  let { username, passwordGuests } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(passwordGuests, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
