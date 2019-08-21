b0d456bb3e29274062c2c3975be36fa3b9ec61ea
unknown <dj.zaragoza@icloud.com>
2019-08-20 19:22:38 -0700

    first commit

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,4 +2,9 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 
+const bcrypt = require('bcryptjs');
+
+const credentials = req.body;
+const hash = bcrypt.hashSync(credentials.password, 14);
+
 module.exports = knex(knexConfig.development);
